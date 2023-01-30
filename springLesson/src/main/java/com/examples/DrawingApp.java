d0da247f2ec340dc1103001316bf59f379e8a611
Jiaxi Xu <xujiaxi@berkeley.edu>
2023-01-30 18:53:27 -0500

    deleted spring.xml

--- springLesson/src/main/java/com/examples/DrawingApp.java
+++ springLesson/src/main/java/com/examples/DrawingApp.java
@@ -9,9 +9,11 @@ public class DrawingApp {
 
 	public static void main(String[] args) {
 //		Triangle t = new Triangle();
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext();
-		Triangle t = context.getBean("triangle",Triangle.class);
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle t = context.getBean("Triangle",Triangle.class);
 		t.draw();
+		Triangle t2 = context.getBean("Triangle2",Triangle.class);
+		t2.draw();
 		context.registerShutdownHook();
 		context.close();
 	}
--- springLesson/src/main/java/com/examples/beans/Triangle.java
+++ springLesson/src/main/java/com/examples/beans/Triangle.java
@@ -1,7 +1,50 @@
 package com.examples.beans;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class Triangle {
-	public void draw() {
-		System.out.println("A Triangle is drawn");
+	
+	private static final Logger LOGGER =  LogManager.getLogger(Triangle.class);
+	
+	private String name;
+	private int height;
+	
+	public Triangle(String name, int height) {
+		super();
+		LOGGER.debug("(name, height) constructor");
+		this.name = name;
+		this.height = height;
 	}
+	
+	public Triangle() {
+		super();
+		LOGGER.debug("default constructor called");
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+		LOGGER.debug("triangle setName called");
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+		LOGGER.debug("triangle setName called");
+	}
+
+	public void draw() {
+		System.out.println(this.getName()+ " triangle is drawn of height "+this.getHeight());
+	}
+
+
+
+
 }
